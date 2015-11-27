c22c4967acafe778fcaa5447aea3fa7760cfda4f
Dmitriy Pashkovsky <pashkovskiy1995@gmail.com>
2015-11-27 13:44:55 +0300

    Main site template have been created

---
+++ local/templates/main/header.php
@@ -0,0 +1,12 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+?>
+<html>
+<head>
+<?$APPLICATION->ShowHead();?>
+</head>
+<body>
+<?$APPLICATION->ShowPanel();?>
+<div id="header"></div>
+<div id="main"></div>
+<div id="footer"></div>
